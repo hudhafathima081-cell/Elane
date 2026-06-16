77b1425a9e9e8519bf7fd22f9dd1b06f37c446c3
hudha081 <hudhafathima081@gmail.com>
2026-06-16 17:54:38 +0530

    updated shipping  navbar

---
+++ app/shipping/page.tsx
@@ -0,0 +1,17 @@
+export default function ShippingPage() {
+  return (
+
+    <div className="min-h-screen bg-[#F8F4EE] p-20">
+
+      <h1 className="text-6xl font-serif mb-10">
+        Shipping Policy
+      </h1>
+
+      <p>
+        We provide free worldwide shipping on all luxury collections.
+      </p>
+
+    </div>
+
+  );
+}
