0208d2b8946f95e03b80d0bef9236147d4f2e441
hudha081 <hudhafathima081@gmail.com>
2026-06-16 17:55:31 +0530

    updated returns  navbar

---
+++ app/returns/page.tsx
@@ -0,0 +1,17 @@
+export default function ReturnsPage() {
+  return (
+
+    <div className="min-h-screen bg-[#F8F4EE] p-20">
+
+      <h1 className="text-6xl font-serif mb-10">
+        Return Policy
+      </h1>
+
+      <p>
+        Returns accepted within 7 days after delivery.
+      </p>
+
+    </div>
+
+  );
+}
