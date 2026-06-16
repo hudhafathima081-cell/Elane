8999b5c2d567810f22ac26ae2acadb4b0086f4e2
hudha081 <hudhafathima081@gmail.com>
2026-06-16 22:11:45 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,35 @@ export default function Home() {
       <Navbar />
 
       <Hero />
+      <section className="relative h-screen overflow-hidden">
+
+  {/* Background Image */}
+  <img
+    src="/model.jpg"
+    alt="Elane Collection"
+    className="absolute inset-0 w-full h-full object-cover"
+  />
+
+  {/* Dark Overlay */}
+  <div className="absolute inset-0 bg-black/30"></div>
+
+  {/* Content */}
+  <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white">
+
+    <h2 className="text-6xl font-light mb-8">
+      Browse Our Latest Products
+    </h2>
+
+    <a
+      href="/collections"
+      className="border border-white px-10 py-4 tracking-[0.2em] uppercase hover:bg-white hover:text-black transition"
+    >
+      Shop All
+    </a>
+
+  </div>
+
+</section>
 
       <Footer />
 
