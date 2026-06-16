13a88acd191d7109bc826dd97438f38ec7e09a73
hudha081 <hudhafathima081@gmail.com>
2026-06-16 17:57:15 +0530

    updated trackorder navbar

---
+++ app/track-order/page.tsx
@@ -0,0 +1,19 @@
+export default function TrackPage() {
+  return (
+
+    <div className="min-h-screen bg-[#F8F4EE] p-20">
+
+      <h1 className="text-6xl font-serif mb-10">
+        Track Your Order
+      </h1>
+
+      <input
+        type="text"
+        placeholder="Enter Order ID"
+        className="border p-4 rounded-xl w-[400px]"
+      />
+
+    </div>
+
+  );
+}
