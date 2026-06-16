653370aad8432e0da83c1a59877b7d3fcbe08fd5
hudha081 <hudhafathima081@gmail.com>
2026-06-16 17:57:50 +0530

    updated currency navbar

---
+++ app/currency/page.tsx
@@ -0,0 +1,25 @@
+export default function CurrencyPage() {
+  return (
+
+    <div className="min-h-screen bg-[#F8F4EE] p-20">
+
+      <h1 className="text-6xl font-serif mb-10">
+        Currency Selector
+      </h1>
+
+      <select className="border p-4 rounded-xl">
+
+        <option>India - INR ₹</option>
+
+        <option>Europe - EUR €</option>
+
+        <option>USA - USD $</option>
+
+        <option>UK - GBP £</option>
+
+      </select>
+
+    </div>
+
+  );
+}
