8dccb149d8f042fcda7e12c3724623bc39b19e05
hudha081 <hudhafathima081@gmail.com>
2026-06-16 17:53:47 +0530

    updated contact navbar

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+export default function ContactPage() {
+  return (
+
+    <div className="min-h-screen bg-[#F8F4EE] p-20">
+
+      <h1 className="text-6xl font-serif mb-10">
+        Contact Us
+      </h1>
+
+      <p>Email: support@elane.com</p>
+
+      <p className="mt-4">
+        Phone: +91 9876543210
+      </p>
+
+    </div>
+
+  );
+}
