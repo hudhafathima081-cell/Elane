cf72b050e3b4e825d5ca8983d9b96d09bfc0246e
hudha081 <hudhafathima081@gmail.com>
2026-06-16 17:52:55 +0530

    updated amount navbar

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function AboutPage() {
+  return (
+
+    <div className="min-h-screen bg-[#F8F4EE] p-20">
+
+      <h1 className="text-6xl font-serif mb-10">
+        About ÉLANE
+      </h1>
+
+      <p className="text-lg leading-9 max-w-3xl">
+        ÉLANE creates timeless luxury accessories
+        crafted with elegance, sophistication and
+        modern beauty.
+      </p>
+
+    </div>
+
+  );
+}
