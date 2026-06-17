004c05a49625efd65e416f16e6e448422a30af21
hudha081 <hudhafathima081@gmail.com>
2026-06-17 18:20:14 +0530

    updated ring page

--- app/collections/rings/page.tsx
+++ app/collections/rings/page.tsx
@@ -37,6 +37,35 @@ export default function RingsPage() {
       <h1 className="text-6xl text-center font-serif mb-20">
         Luxury Rings
       </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-10">
+
+  {rings.map((ring, index) => (
+
+    <div key={index} className="text-center">
+
+      <img
+        src={ring.image}
+        alt={ring.name}
+        className="w-full h-[350px] object-cover rounded-[30px]"
+      />
+
+      <h2 className="mt-5 text-3xl font-serif">
+        {ring.name}
+      </h2>
+
+      <p className="mt-2 text-[#9E2F2F] text-xl">
+        {ring.price}
+      </p>
+
+      <button className="mt-5 bg-black text-white px-8 py-4 rounded-full">
+        ADD TO CART
+      </button>
+
+    </div>
+
+  ))}
+
+</div>
     </div>
   );
 }
