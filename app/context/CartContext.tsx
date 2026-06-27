ad221d90dab58f23f6976630330e79e4f9b2328b
hudha081 <hudhafathima081@gmail.com>
2026-06-27 22:47:36 +0530

    updated cart button

--- app/context/CartContext.tsx
+++ app/context/CartContext.tsx
@@ -1,12 +1,30 @@
 "use client";
 
 import { createContext, useContext, useState } from "react";
+import { useEffect } from "react";
+
+
 
 const CartContext = createContext<any>(null);
 
 export function CartProvider({ children }: any) {
   const [cartItems, setCartItems] = useState<any[]>([]);
 
+useEffect(() => {
+  const savedCart = localStorage.getItem("cart");
+
+  if (savedCart) {
+    setCartItems(JSON.parse(savedCart));
+  }
+}, []);
+
+useEffect(() => {
+  localStorage.setItem("cart", JSON.stringify(cartItems));
+}, [cartItems]);
+
+useEffect(() => {
+  localStorage.setItem("cart", JSON.stringify(cartItems));
+}, [cartItems]);
   const addToCart = (product: any) => {
     setCartItems((prev) => [...prev, product]);
   };
