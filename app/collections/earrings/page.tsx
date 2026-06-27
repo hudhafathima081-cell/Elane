203201fc829fbb95d3e967017ce7fe0ea343d71b
hudha081 <hudhafathima081@gmail.com>
2026-06-27 19:01:41 +0530

    fixed search page

--- app/collections/earrings/page.tsx
+++ app/collections/earrings/page.tsx
@@ -5,42 +5,42 @@ export default function EarringsPage() {
   const earrings = [
 
     {
-      name: "Saint Laurent Noir",
+      name: "Aurora Blossom Pearl Earrings",
       image: "https://i.pinimg.com/736x/0a/2b/f7/0a2bf71ddb4ea43834892ca416e2dfeb.jpg",
       price: "₹18,999",
       slug: "saint-laurent-noir",
     },
 
     {
-      name: "Gucci Riviera",
+      name: "Celeste Pearl Cascade Earrings",
       image: "https://i.pinimg.com/1200x/99/17/1b/99171b030ac6859f3d0bf6d5664eb498.jpg",
       price: "₹22,999",
       slug: "gucci-riviera",
     },
 
     {
-      name: "Prada Eclipse",
+      name: "Crystal Wreath Stud Earrings",
       image: "https://i.pinimg.com/736x/cd/d0/67/cdd0673bce2e2156f77021dd72e68972.jpg",
       price: "₹25,999",
       slug: "prada-eclipse",
     },
 
     {
-      name: "Tom Ford Prestige",
+      name: "Celeste Pearl Ear Cuff",
       image: "https://amalfa.in/cdn/shop/files/pearl-crystal-ear-cuff-earrings_for_women.jpg?v=1776405010&width=800",
       price: "₹27,999",
       slug: "tom-ford-prestige",
     },
 
     {
-      name: "Versace Royale",
+      name: "Royal Diamond Ear Stack",
       image: "https://i.pinimg.com/736x/ed/f2/cf/edf2cf564a3ac6d6339744384095b214.jpg",
       price: "₹24,999",
       slug: "versace-royale",
     },
 
     {
-      name: "Cartier Lumière",
+      name: "Aurelia Twist Earrings",
       image: "https://i.pinimg.com/736x/fd/79/59/fd7959e7080bd4b834a0814119f8feba.jpg",
       price: "₹31,999",
       slug: "cartier-lumiere",
---
+++ app/search/page.tsx
@@ -0,0 +1,9 @@
+export default function SearchPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-serif">
+        Search Page
+      </h1>
+    </div>
+  );
+}
