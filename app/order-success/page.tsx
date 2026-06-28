e3db0ce2f7bbfb7047863e18ed4e9802a81f3443
hudha081 <hudhafathima081@gmail.com>
2026-06-28 22:03:54 +0530

    updated code

---
+++ app/order-success/page.tsx
@@ -0,0 +1,15 @@
+export default function OrderSuccessPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#F8F4EE]">
+      <div className="bg-white rounded-3xl p-12 shadow-lg text-center">
+        <h1 className="text-5xl font-serif mb-4">
+          Order Successful 🎉
+        </h1>
+
+        <p className="text-gray-600 text-lg">
+          Thank you for shopping with ÉLANE.
+        </p>
+      </div>
+    </div>
+  );
+}
