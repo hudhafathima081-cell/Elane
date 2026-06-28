0370ef5a76bc5322f0a638b827186f4eee020ab7
hudha081 <hudhafathima081@gmail.com>
2026-06-28 21:29:33 +0530

    created checkout page

---
+++ app/checkout/page.tsx
@@ -0,0 +1,7 @@
+export default function CheckoutPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl">Checkout Page</h1>
+    </div>
+  );
+}
